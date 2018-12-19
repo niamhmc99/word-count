6caef0bede67df9800efb44db4b880399dc26591
niamhmc99 <43484599+niamhmc99@users.noreply.github.com>
2018-12-19 19:32:54 +0000

    Complete

--- word_count.py
+++ word_count.py
@@ -1,12 +1,44 @@
+import string
 from collections import Counter
 
 
-def word_count(phrase):
+def word_count(s):
+   	# remove punctuation
+	s = s.lower()
+	for char in string.punctuation:
+		s = s.replace(char, " ")
+	s = s.replace('\n', ' ').replace('\t', ' ')
 
-    word_including_apostrophe = compile(r"([a-z\d]+(?:'[a-z\d]+)?)") 
-    words = word_including_apostrophe.findall(phrase.lower()) 
-    counter = Counter(words) 
-    return dict(counter)
+	# split into list 
+	s = ''.join(s)
+	s = s.split(' ')
+	
+	# count words, remove empty strings
+	# and put in a dict
+	result = {}
+	for word in s:
+		if word == ' ' or word == '':
+			continue
+		else:
+			key = s.count(word)
+			result[word] = key
+	
+	return result
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    #return Counter(phrase.translate(None, string.punctuation).lower().split())
+    #word_including_apostrophe = compile(r"([a-z\d]+(?:'[a-z\d]+)?)") 
+    #words = word_including_apostrophe.findall(phrase.lower()) 
+    #counter = Counter(words) 
+    #return dict(counter)
+
   
     # for char in ('-.,\n'):
     #     phrase = phrase.replace(char, ' ')
